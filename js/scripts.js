6cefc6a3036b2e4700e7121583d854507fdfd9f3
Miguel Briseño <miguel.briseno.bustos@gmail.com>
2021-10-05 21:04:28 -0500

    Añadimos movimiento a la pelota

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
 'use strict'
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
 
-ctx.beginPath();
-ctx.rect(0, 0, 100, 20);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x, y, 5, 0, Math.PI * 2);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.closePath();
+}
 
-ctx.beginPath();
-ctx.arc(80, 80, 5, 0, 2 * Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "white";
-ctx.stroke();
-ctx.closePath();
+setInterval(draw, 15);
